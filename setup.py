b659b125e42091fa1b63a43bdd30f4d0fa304666
Jaco du Plessis <jaco@jacoduplessis.co.za>
2019-12-02 13:56:14 +0200

    Update ZA inflation numbers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cpi_inflation',
-    version='2019.11.1',
+    version='2019.12.0',
     url='https://github.com/jacoduplessis/cpi_inflation',
     author='Jaco du Plessis',
     author_email='jaco@jacoduplessis.co.za',
